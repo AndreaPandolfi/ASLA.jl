96345454017d34ae968da4b39364e430dbd8aff8
Andrea Pandolfi <3182891@phd.unibocconi.it>
2024-08-29 17:23:23 +0200

    Renamed I to G

--- src/random_simulation.jl
+++ src/random_simulation.jl
@@ -14,8 +14,8 @@ function _build_precision_and_preconditioned_precision(V::SparseMatrixCSC{Tv,Ti}
     return Q, norm_Q
 end#
 
-function _build_V(I::Vector{R}; precisions=Dict("T_0" => 0.0, "T" => nothing), Tv=Float64, Ti=Int64) where{R<:Integer}
-    K = length(I)
+function _build_V(G::Vector{R}; precisions=Dict("T_0" => 0.0, "T" => nothing), Tv=Float64, Ti=Int64) where{R<:Integer}
+    K = length(G)
 
     T_0 = get(precisions, "T_0", -1)
     T = get(precisions, "T", -1)
@@ -24,9 +24,9 @@ function _build_V(I::Vector{R}; precisions=Dict("T_0" => 0.0, "T" => nothing), T
     @assert length(T) == K
 
     # build V
-    diag_V = Vector{Tv}(undef, sum(I)+1)
+    diag_V = Vector{Tv}(undef, sum(G)+1)
     index = 0
-    for (i, t) in zip(I, T)
+    for (i, t) in zip(G, T)
         diag_V[(index+1):(index+i)] .= t
         index += i
     end
@@ -36,54 +36,53 @@ function _build_V(I::Vector{R}; precisions=Dict("T_0" => 0.0, "T" => nothing), T
 end #
 
 # ER K-partite graph
-function _remove_unobserved_levels(Z::SparseMatrixCSC{Tv, Ti}, I::Vector{R}; verbose::Bool=false) where{Tv, Ti, R}
-    # Needs the I::Vector, to change the n. of levels accordingly
-    sel = vec(sum(Z, dims=2).>0)
+function _remove_unobserved_levels(Z::SparseMatrixCSC{Tv, Ti}, G::Vector{R}; verbose::Bool=false) where{Tv, Ti, R}
+    # Needs the G(Z, dims=2).>0)
 
-    ptr = cumsum(vcat(1, I))
-    I .= eltype(I)[sum(sel[ptr[i]:ptr[i+1]-1]) for i in 1:length(I)] # updated I
+    ptr = cumsum(vcat(1, G))
+    G .= eltype(G)[sum(sel[ptr[i]:ptr[i+1]-1]) for i in 1:length(G)] # updated G
     verbose && println("Removed $(size(Z, 1)-sum(sel)) out of $(size(Z, 1)) levels")
     return Z[sel, :] 
 end #
 
-function _generate_ER_U(N::Integer, I::Vector{R}; kwargs...) where {R<:Integer}
-    Z = _generate_ER_Z(N, I; kwargs...)
+function _generate_ER_U(N::Integer, G::Vector{R}; kwargs...) where {R<:Integer}
+    Z = _generate_ER_Z(N, G; kwargs...)
     return Z * transpose(Z)
 end #
 
-function _generate_ER_U(I::Vector{R}, π::AbstractFloat; kwargs...) where{R<:Integer}
-    K_dim_array_size = prod(I)
+function _generate_ER_U(G::Vector{R}, π::AbstractFloat; kwargs...) where{R<:Integer}
+    K_dim_array_size = prod(G)
     N = rand(Binomial(K_dim_array_size, π))
 
-    Z = _generate_ER_Z(N, I; kwargs...)
+    Z = _generate_ER_Z(N, G; kwargs...)
     return Z * transpose(Z)
 end #
 
-function _generate_ER_Z(N::Integer, I::Vector{R}; remove_unobserved_levels::Bool=false, Tv=Float64, Ti=Int64, seed=nothing, debug::Bool=false) where{R<:Integer}
+function _generate_ER_Z(N::Integer, G::Vector{R}; remove_unobserved_levels::Bool=false, Tv=Float64, Ti=Int64, seed=nothing, debug::Bool=false, ordered::Bool=false) where{R<:Integer}
     if seed !== nothing
         Random.seed!()
     end
     
-    p = sum(I) + 1
-    K = length(I) 
-    K_dim_array_size = prod(I) 
+    p = sum(G) + 1
+    K = length(G) 
+    K_dim_array_size = prod(G) 
 
     Z_colptr = [1+i*(K+1) for i in range(0, N)]
     Z_rowval = Array{Ti}(undef, (K+1)*N)
     Z_nzval = ones(Tv, length(Z_rowval))
 
-    samples_from_mult_array = sample(1:K_dim_array_size, N, replace=false)
+    samples_from_mult_array = sample(1:K_dim_array_size, N, replace=false, ordered=ordered)
 
-    cartesian = CartesianIndices(Tuple(I))
+    cartesian = CartesianIndices(Tuple(G))
 
     for (sample, ptr) in zip(samples_from_mult_array, Z_colptr[1:(end-1)])
         levels = collect(Tuple(cartesian[sample]))
-        Z_rowval[ptr: (ptr+K)] .= convert(Vector{Ti}, cumsum(vcat(0, I)).+ vcat(levels, 1))
+        Z_rowval[ptr: (ptr+K)] .= convert(Vector{Ti}, cumsum(vcat(0, G)).+ vcat(levels, 1))
 
         debug && println(levels)
     end
 
     Z = SparseMatrixCSC{Tv, Ti}(p, N, Z_colptr, Z_rowval, Z_nzval)
-    remove_unobserved_levels && return _remove_unobserved_levels(Z, I)
+    remove_unobserved_levels && return _remove_unobserved_levels(Z, G)
     return Z
 end #
